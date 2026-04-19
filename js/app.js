90c61e4f0d6fa2e0636ec6f1bc2d808b8b6f1ea7
KrzysztofCwikla1 <cwikakrzysztof@gmail.com>
2026-04-19 13:24:56 +0200

    gh

--- js/app.js
+++ js/app.js
@@ -71,10 +71,7 @@ function handleFile(file) {
 }
 cameraInput.addEventListener('change', e => handleFile(e.target.files[0]));
 galleryInput.addEventListener('change', e => handleFile(e.target.files[0]));
-reportText.addEventListener('input', (e) => {
-    currentReportText = e.target.value;
-    enableShare();
-});
+
 //GEOLOKALIZACJA
 const locationModal = new bootstrap.Modal(document.getElementById('locationModal'));
 
